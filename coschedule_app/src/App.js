5352673d3962bcf71dd7770304b4cad887678fc2
priyankadora20 <prdora97@gmail.com>
2022-10-01 13:48:12 +0530

    day3

---
+++ coschedule_app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import {LandingPage} from './Pages/LandingPage';
+
+function App() {
+  return (
+    <div className='App'>
+      <LandingPage />
+      
+    </div>
+  );
+}
+
+export default App;
